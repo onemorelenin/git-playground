9ede13f1ad84f994245548e60f739a2afd87c986
onemorelenin <leonidkadnikoff@gmail.com>
2022-12-01 15:32:39 +0200

    first-improvement

--- main.py
+++ main.py
@@ -44,6 +44,10 @@ print(f"Your word is '{word}'")
 while not is_game_over():
     guess = input("Your next take: ")
 
+    if guess in guesses:
+        print("This word has already been mentioned")
+        continue
+
     if not guess_is_valid(guess):
         continue
 
